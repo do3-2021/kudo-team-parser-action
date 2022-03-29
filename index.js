647440c8423d1b5bf19ad03b02cbc4dceb191f76
Simon Lucido <simon.lucido@etu.umontpellier.fr>
2022-03-29 14:43:12 +0200

    feat: add PR parsing
    Signed-off-by: Simon Lucido <simon.lucido@etu.umontpellier.fr>

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+let requestBody;
+let finalBody;
+let number;
+
+const githubToken = core.getInput("github_token");
+const client = github.getOctokit(githubToken);
+
 const getPullRequestBody = (context) => {
     const { payload: { pull_request } } = context;
     
@@ -27,9 +34,6 @@ const findLabelsInBody = (body) => {
     }
 
     const labels = body.split('\n').reduce( (acc, curr, currentIndex, array) => {
-        console.log(`Current line: ${curr}`);
-        console.log(`Current labels: ${acc}`);
-        
         if ( curr.includes("Which teams are related") || curr.includes("Is it a team issue or a project issue")) {
             if ( !Array.isArray(acc) )
                 return array[currentIndex+2].split(',').map( (label) => label.trim().toLowerCase());
@@ -45,29 +49,86 @@ const findLabelsInBody = (body) => {
     return labels;
 }
 
+const parseTaskList = (taskList) => {
+    return taskList.reduce((previous, current) => {
+        if (current.includes('- [x]')) {
+            previous.push(current.replace('- [x]', '').trim());
+        }
+        return previous;
+    }, []);
+}
+
+const addLabels = (labels) => {
+    if ( labels ) {
+        client.rest.issues.addLabels({
+            ...requestBody,
+            issue_number: number,
+            labels
+        });
+    }
+}
+
+const parsePRBody = (body) => {
+    const lines = body.split('\n');
+    return [
+    lines[3].trim().toLowerCase(), 
+    lines[4].trim().toLowerCase(), 
+    lines[7].trim().toLowerCase(), 
+    lines[8].trim().toLowerCase(), 
+    lines[9].trim().toLowerCase(), 
+    lines[10].trim().toLowerCase(), 
+    lines[11].trim().toLowerCase()];
+}
+
+const removeHead = (issue = true) => {
+    if ( issue ) {
+        client.rest.issues.update({
+            ...requestBody,
+            issue_number: number,
+            body: finalBody
+        });
+    } else {
+        client.rest.pulls.update({
+            ...requestBody,
+            pull_number: number,
+            body: finalBody
+        });
+    }
+}
+
 try {
-    const githubToken = core.getInput("github_token");
-    const client = github.getOctokit(githubToken);
-    const body = getIssueBody(github.context) || getPullRequestBody(github.context);
+    const issueBody = getIssueBody(github.context)
+    const prBody = getPullRequestBody(github.context);
+    const body = issueBody || prBody;
 
     if (body) {
-        const labels = findLabelsInBody(body);
+        let labels;
 
-        if (labels) {
-            client.rest.issues.addLabels({
+        if ( issueBody ) {
+            labels = findLabelsInBody(body);
+            requestBody = {
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
-                issue_number: github.context.issue.number,
-                labels,
-            });
+            };
+
+            number = github.context.issue.number;
+            finalBody = body.split('\n').slice(8).join('\n'); 
+        } else {
+            console.log(`parsedBody : ${parsePRBody(prBody)}`);
+            labels = parseTaskList(parsePRBody(prBody));
+            requestBody = {
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+            };
+            
+            number = github.context.payload.pull_request.number;
+            finalBody = body.split('\n').slice(12).join('\n');
         }
 
-        client.rest.issues.update({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: github.context.issue.number,
-            body: body.split('\n').slice(8).join('\n'), // remove the first 8 lines
-        });
+        addLabels(labels);
+        removeHead();
+
+
     } else {
         core.setFailed("No body found");
     }
